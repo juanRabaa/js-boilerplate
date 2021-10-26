0f478b6256eec238e9584c2f84b550c42ea74c8d
Juan Cruz Rabaglia <juanrabaa@gmail.com>
2021-10-26 12:27:32 -0300

    seed

---
+++ config/webpack.common.js
@@ -0,0 +1,87 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const PrettierPlugin = require('prettier-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const paths = require('./paths');
+
+module.exports = {
+    // Where webpack looks to start building the bundle
+    entry: [paths.src + '/index.js'],
+
+    // Where webpack outputs the assets and bundles
+    output: {
+        path: paths.build,
+        filename: '[name].bundle.js',
+        publicPath: '/',
+    },
+
+    // Customize the webpack build process
+    plugins: [
+        // ESLint configuration
+        new ESLintPlugin({
+            files: ['.', 'src', 'config'],
+            formatter: 'table',
+        }),
+
+        // Removes/cleans build folders and unused assets when rebuilding
+        new CleanWebpackPlugin(),
+
+        // Copies files from target to destination folder
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: paths.public,
+                    to: 'assets',
+                    globOptions: {
+                        ignore: ['*.DS_Store'],
+                    },
+                    noErrorOnMissing: true,
+                },
+            ],
+        }),
+
+        // Generates an HTML file from a template
+        // Generates deprecation warning: https://github.com/jantimon/html-webpack-plugin/issues/1501
+        new HtmlWebpackPlugin({
+            title: 'Universo Genealógico de la Creatividad Argentina #UniversoCCA',
+            favicon: paths.src + '/assets/images/favicon.png',
+            template: paths.src + '/template.html', // template file
+            filename: 'index.html', // output file
+        }),
+
+        // Prettier configuration
+        new PrettierPlugin({
+            tabWidth: 4,
+            useTabs: false,
+            semi: true,
+        }),
+    ],
+
+    // Determine how modules within the project are treated
+    module: {
+        rules: [
+            // JavaScript: Use Babel to transpile JavaScript files
+            { test: /\.js$/, use: ['babel-loader'] },
+
+            // Images: Copy image files to build folder
+            { test: /\.(?:ico|gif|png|jpg|jpeg)$/i, type: 'asset/resource' },
+
+            // Fonts and SVGs: Inline files
+            { test: /\.(woff(2)?|eot|ttf|otf|svg|)$/, type: 'asset/inline' },
+        ],
+    },
+
+    resolve: {
+        // Must be mapped in .eslintrc.json import/resolver
+        alias: {
+            JS: `${paths.src}/js`,
+            HELPERS: `${paths.src}/js/helpers`,
+            CONSTS: `${paths.src}/js/consts`,
+            FONTS: `${paths.src}/assets/fonts`,
+            ASSETS: `${paths.src}/assets`,
+            IMAGES: `${paths.src}/assets/images`,
+        },
+        extensions: ['.js', '.jsx'],
+    },
+};
---
+++ config/webpack.dev.js
@@ -0,0 +1,58 @@
+const webpack = require('webpack');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const paths = require('./paths');
+
+module.exports = merge(common, {
+    // Set the mode to development or production
+    mode: 'development',
+
+    // Control how source maps are generated
+    devtool: 'inline-source-map',
+
+    // Spin up a server for quick development
+    devServer: {
+        historyApiFallback: true,
+        contentBase: paths.build,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Cross-Origin-Embedder-Policy": "require-corp",
+            "Cross-Origin-Opener-Policy": "same-origin",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+            "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+        },
+        open: true,
+        compress: true,
+        hot: true,
+        port: 8080,
+    },
+
+    module: {
+        rules: [
+            // Styles: Inject CSS into the head with source maps
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                            importLoaders: 1,
+                            modules: {
+                                auto: true,
+                            },
+                        },
+                    },
+                    { loader: 'postcss-loader', options: { sourceMap: true } },
+                    { loader: 'sass-loader', options: { sourceMap: true } },
+                ],
+            },
+        ],
+    },
+
+    plugins: [
+        // Only update what has changed on hot reload
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+});
